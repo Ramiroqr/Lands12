f6fca76b8f8ffc261da47287f045dadf934a728a
Ramiroqr <ramiroquisper@gmail.com>
2019-09-24 12:43:34 -0500

    LandItemViewModel

---
+++ Lands12/Lands/Lands/ViewModels/LandViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Lands.ViewModels
+{
+    using Lands.Models;
+    class LandViewModel
+    {
+        #region Properties
+        public Land Land
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Consructors
+        public LandViewModel(Land land)
+        {
+            this.Land = land;
+        } 
+        #endregion
+    }
+}
--- Lands12/Lands/Lands/ViewModels/LandsViewModel.cs
+++ Lands12/Lands/Lands/ViewModels/LandsViewModel.cs
@@ -17,7 +17,7 @@
         #endregion
 
         #region Attributes
-        private ObservableCollection<Land> lands;
+        private ObservableCollection<LandItemViewModel> lands;
         private bool isRefreshing;
         private string filter;
         private List<Land> landsList;
@@ -25,7 +25,7 @@
 
         #region Properties
         //Las listView acceptan listas del tipo ObservableCollection
-        public ObservableCollection<Land> Lands
+        public ObservableCollection<LandItemViewModel> Lands
         {
             get { return this.lands; }
             set { SetValue(ref this.lands, value); }
@@ -85,22 +85,54 @@
                 return;
             }
             landsList = (List<Land>)response.Result;
-            this.Lands = new ObservableCollection<Land>(this.landsList);
+            this.Lands = new ObservableCollection<LandItemViewModel>(
+                this.ToLandItemViewModel());
             this.IsRefreshing = false;
         }
 
+        private IEnumerable<LandItemViewModel> ToLandItemViewModel()
+        {
+            return this.landsList.Select(l => new LandItemViewModel()
+            {
+                Alpha2Code = l.Alpha2Code,
+                Alpha3Code = l.Alpha3Code,
+                AltSpellings = l.AltSpellings,
+                Area = l.Area,
+                Borders = l.Borders,
+                CallingCodes = l.CallingCodes,
+                Capital = l.Capital,
+                Cioc = l.Cioc,
+                Currencies = l.Currencies,
+                Demonym = l.Demonym,
+                Flag = l.Flag,
+                Gini = l.Gini,
+                Languages = l.Languages,
+                Latlng = l.Latlng,
+                Name = l.Name,
+                NativeName = l.NativeName,
+                NumericCode = l.NumericCode,
+                Population = l.Population,
+                Region = l.Region,
+                RegionalBlocs = l.RegionalBlocs,
+                Subregion = l.Subregion,
+                Timezones = l.Timezones,
+                TopLevelDomain = l.TopLevelDomain,
+                Translations = l.Translations
+            });
+        }
 
         private void Search()
         {
             if(string.IsNullOrEmpty(this.Filter))
             {
-                this.Lands = new ObservableCollection<Land>(this.landsList);
+                this.Lands = new ObservableCollection<LandItemViewModel>(
+                this.ToLandItemViewModel());
             }
             else
             {
-                this.Lands = new ObservableCollection<Land>(
-                    this.landsList.Where(l => l.Name.ToLower().Contains(this.Filter.ToLower()) ||
-                                              l.Capital.ToLower().Contains(this.Filter.ToLower())));
+                this.Lands = new ObservableCollection<LandItemViewModel>(
+                    this.ToLandItemViewModel().Where(l => l.Name.ToLower().Contains(this.Filter.ToLower()) ||
+                                                          l.Capital.ToLower().Contains(this.Filter.ToLower())));
             }
         }
         #endregion
